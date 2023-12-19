8dffb1ae0f24f41a50ded9e7326972c721ac5be7
MrTvister99 <154116388+MrTvister99@users.noreply.github.com>
2023-12-19 11:46:38 +0300

    Add files via upload

---
+++ Mi-main (1)/Mi-main/Mi/WpfApp1/WpfApp1/Korzina.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для Korzina.xaml
+    /// </summary>
+    public partial class Korzina : Page
+    {
+        private List<string> TowarList1 = new List<string>();
+        public string Name2;
+        string server = "10.112.48.11";
+        string database = "lesuser9";
+        string username = "lesuser9";
+        string passwordDB = "lesuser9";
+        public Korzina()
+        {
+            InitializeComponent();
+          podkl();
+
+        }
+        private void podkl()
+        {
+            
+            using (SqlConnection connection = new SqlConnection($"Server={server};Database={database};User ID={username};Password={passwordDB}"))
+            {
+
+                //login = email.Text;
+                //password = Password.Text;
+                connection.Open();
+
+                string sql = "SELECT * FROM Korzina";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+
+                            Name2 = reader["product"].ToString();
+                            TowarList1.Add($" {Name2}");
+
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            TowarListView1.ItemsSource = TowarList1;
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            using (SqlConnection connection = new SqlConnection($"Server={server};Database={database};User ID={username};Password={passwordDB}"))
+            {
+                // Предполагается, что TowarListView1 возвращает строку. Если нет, этот код нужно изменить.
+                if (TowarListView1.SelectedItem == null)
+                {
+                    MessageBox.Show("No item selected");
+                    return;
+                }
+
+                // Предположим, что selectedItemText получает данные напрямую из выбранного элемента в ListView
+                string selectedItemText = TowarListView1.SelectedItem.ToString().Trim();
+                if (string.IsNullOrEmpty(selectedItemText))
+                {
+                    MessageBox.Show("The selected item is invalid");
+                    return;
+                }
+                selectedItemText = " " + selectedItemText;
+                connection.Open();
+
+                using (SqlTransaction transaction = connection.BeginTransaction())
+                {
+                    // Параметризованный запрос без пробела внутри литерала строки SQL
+                    string sql = "DELETE FROM Korzina WHERE product = @product";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        command.Transaction = transaction;
+                        // Добавляем параметр без пробелов
+                        command.Parameters.AddWithValue("@product", selectedItemText);
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            transaction.Commit();
+                            MessageBox.Show($"{selectedItemText} deleted");
+                            
+          
+                        }
+                        
+                    
+                        else
+                        {
+                            transaction.Rollback();
+                            MessageBox.Show("No rows affected. Item might not exist in the database.");
+                        }
+                    }
+                }
+            }
+            
+        }
+            private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Menu());
+        }
+
+        private void info_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Info());
+        }
+
+        private void Out(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Page1());
+        }
+    }
+}
---
+++ Mi-main (1)/Mi-main/Mi/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string server = "10.112.48.11";
+        string database = "lesuser9";
+        string username = "lesuser9";
+        string passwordDB = "lesuser9";
+        public bool found;
+        
+        
+        public string email1;
+        public string password1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void podkl()
+        {
+           
+            using (SqlConnection connection = new SqlConnection($"Server={server};Database={database};User ID={username};Password={passwordDB}"))
+            {
+                connection.Open();
+
+                string sql = "SELECT * FROM Users";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        bool found = false;
+                        while (reader.Read())
+                        {
+                            email1 = reader["email"].ToString();
+                            password1 = reader["password"].ToString();
+                            password1 = password1.Trim();
+                            email1 = email1.Trim();
+
+                            if (password1 == Password.Text && email.Text == email1)
+                            {
+                                found = true;
+                            }
+                        }
+
+                        if (Password.Text != "" && email.Text != "")
+                        {
+                            if (found)
+                            {
+                                frame.Navigate(new Menu());
+                            }
+                            else
+                            {
+                                MessageBox.Show($"Неправильный логин или пароль");
+                            }
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Поля не могут быть пустыми");
+                        }
+                    }
+                }
+            }
+        }
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            podkl();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            frame.Navigate(new Page1());
+        }
+    }
+}
